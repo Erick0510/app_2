f2d0d58393a61aef532dffeae736c2ca629d242e
Erick0510 <115128397+Erick0510@users.noreply.github.com>
2023-01-13 14:40:46 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>MI PRACTICA CON CODESANBOX</h1>
+<div>
+  <p>ERICK YAHIR MERAZ SOLIS</p>
+  UNIVERSIDAD TECNOLOGICA DE DURANGO 
+  <a href="https://github.com/Erick0510/app_1.git" target="_blank" rel="noopener noreferrer">MI GITHUB</a>.
+</div>
+`;
